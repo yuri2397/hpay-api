05c49feaa6b413a704bfb776545382a5be5ff204
mordiaw <diaw@tanelhealth.com>
2025-03-30 02:02:12 +0000

    Enhance CmaCgmInvoiceController by implementing payInvoice method and refining invoice handling logic. Update EmailVerificationController to improve password reset functionality with enhanced validation. Streamline NotificationController for better request parameter validation. Expand UserController with additional user management methods, ensuring a more robust user experience.

---
+++ database/migrations/2025_03_29_225305_create_user_activities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_activities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained('users');
+            $table->string('type')->default('login');
+            $table->datetime('started_at')->nullable()->default(now());
+            $table->datetime('ended_at')->nullable();
+            $table->json('data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_activities');
+    }
+};
